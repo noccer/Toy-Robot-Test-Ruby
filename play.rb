37d64b19f5389f724664281cb1ae241eb79fe951
Niall <noccer@yahoo.ie>
2016-08-10 20:19:31 +1000

    successfully checking if a robot exists, and then placing the robot in the co-ordinates initially stated. next step is to separate out a create function on its own.

--- play.rb
+++ play.rb
@@ -1,8 +1,12 @@
 require './robot.rb'
 require './table.rb'
 
-def create_robot
-  robot = Robot.new #initialise a blank robot which is not placed on the board yet
+# def create_robot(x,y,f)
+#   robot = Robot.new(x,y,f)
+def create_robot(x)
+  robot = Robot.new(x)
+  "ran create_robot, robot created"
+  puts robot.inspect
   @robot = robot
 end
 
@@ -10,8 +14,10 @@ def print_menu
 	puts "*****************"
 	puts "NIALL'S TOY ROBOT"
 	puts "Choose an option"
+  puts "MAKE"
 	puts "PLACE"
 	puts "MOVE"
+  puts "REPORT"
 	print "Selection: "
 	gets.chomp.to_s.upcase
 end
@@ -20,14 +26,29 @@ while true
 	selection = print_menu
 	case selection
 	when "PLACE"
+    puts "please enter robot x-cordinates (number)"
+    robot_x = gets.chomp
     if @robot
-      create_robot
+      puts "Robot exists, placing robot"
+      @robot.place(robot_x)
+      puts "Robot created at #{robot_x}"
+    else
+      puts "Robot exists, creating robot"
+      create_robot(robot_x)
+      @robot.place(robot_x)
+      puts "Robot created at #{robot_x}"
     end
-		@robot.place
 	when "MOVE"
     create_robot
 		@robot.move
+  when "REPORT"
+    if @robot
+      puts @robot.inspect
+    else
+      puts "There is no Robot yet, why not make one!?"
+    end
 	else
+    puts "exiting"
 		exit
 	end
 end
