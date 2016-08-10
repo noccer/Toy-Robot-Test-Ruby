2d6190806a3af18537041fc5bf4f1d1a610019bd
Niall <noccer@yahoo.ie>
2016-08-10 18:11:47 +1000

    More testing and getting up to speed.

--- robot.rb
+++ robot.rb
@@ -1,21 +1,36 @@
 class Robot
+  attr_accessor :initialize, :move, :place
 
-  def place(x,y,f)
+  def initialize()
+    # @x = x
+    # @y = y
+    # @f = f
+  end
+
+  # def location
+  # end
+  #
+  # def direction
+  # end
+
+  def place(x)
+    puts "Robot Placed"
   end
 
   def move
+    puts "Robot Moved"
   end
 
-  def left
-  end
-
-  def right
-  end
-
-  def report
-  end
-
-  def remove
-  end
+  # def left
+  # end
+  #
+  # def right
+  # end
+  #
+  # def report
+  # end
+  #
+  # def remove
+  # end
 
 end
