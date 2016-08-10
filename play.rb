171424a0337afd0656b3d6395faba1b4b72c9764
Niall <noccer@yahoo.ie>
2016-08-10 18:15:33 +1000

    created a print menu to let users know what options are available.

--- play.rb
+++ play.rb
@@ -5,3 +5,13 @@ def create_robot
   robot = Robot.new #initialise a blank robot which is not placed on the board yet
   @robot = robot
 end
+
+def print_menu
+	puts "*****************"
+	puts "NIALL'S TOY ROBOT"
+	puts "Choose an option"
+	puts "PLACE"
+	puts "MOVE"
+	print "Selection: "
+	gets.chomp.to_s.upcase
+end
