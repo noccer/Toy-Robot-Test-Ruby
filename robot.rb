6bf62b3a6cc2f0914876926cda84f556a7b1d835
Niall <noccer@yahoo.ie>
2016-08-15 19:55:56 +1000

    Make a 'move' method that checks direction then applies the modifier to the robot provided that the robot is not at the edge of the table.

--- robot.rb
+++ robot.rb
@@ -43,4 +43,17 @@ class Robot
       end
     end
 
+  def move(robot, table)
+    check_direction
+    if (robot.x < table.width && robot.x > 1 && robot.y < table.height && robot.y > 1)
+      robot.x = robot.x + @x_modifier
+      robot.y = robot.y + @y_modifier
+      puts "Your robot has moved #{robot.f} by 1 place"
+      puts "Your robot is now at co-ordinates #{robot.x},#{robot.y} and is facing #{robot.f}"
+    else
+      puts "STOP! Your robot is at the edge of the table! Turn your robot LEFT or RIGHT to continue playing."
+      puts "Your robot is at co-ordinates (#{robot.x},#{robot.y}) and is facing #{robot.f}"
+      puts "The table is #{table.width} units wide (EAST-WEST), and #{table.height} units high (NORTH-SOUTH)"
+    end
+  end
 end
