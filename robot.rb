fd3492c6778cd1e9851be044d48ec56de9f3560c
Niall <noccer@yahoo.ie>
2016-08-15 19:52:31 +1000

    Clean up the Model.

--- robot.rb
+++ robot.rb
@@ -9,32 +9,10 @@ class Robot
     @f = f
   end
 
-  # def location
-  # end
-  #
-  # def direction
-  # end
-
   def place(x, y, f)
     @x = x
     @y = y
     @f = f
   end
-
-  def move
-    puts "Robot Moved"
-  end
-
-  # def left
-  # end
-  #
-  # def right
-  # end
-  #
-  # def report
-  # end
-  #
-  # def remove
-  # end
 
 end
