3546cb63e4dd1267b170f9f59a6b9ac992392c8a
Niall <noccer@yahoo.ie>
2016-08-17 01:03:57 +1000

    Moved the grid resize logic from the Play class to the correct location in Table class.

--- table.rb
+++ table.rb
@@ -6,7 +6,35 @@ class Table
     @height = height
   end
 
-  def table_resize(width,height)
+  @grid_edit_mode_width  = true
+  @grid_edit_mode_height = true
 
+  def update_grid_value(table, width_or_height_string, new_table_width_or_height_value)
+    if width_or_height_string == 'width'
+      table.width = new_table_width_or_height_value
+    elsif width_or_height_string == 'height'
+      table.height = new_table_width_or_height_value
+    end
   end
+
+  def set_grid_logic(table, grid_edit_mode, width_or_height_string)
+    while grid_edit_mode == true
+      puts "Please enter your new desired table #{width_or_height_string}:"
+      new_table_width_or_height_value = gets.chomp.to_s.to_i
+      if new_table_width_or_height_value > 0
+        update_grid_value(table, width_or_height_string, new_table_width_or_height_value)
+        grid_edit_mode = false
+      else
+        while new_table_width_or_height_value < 1
+          puts "Please enter a #{width_or_height_string} value greater than 0:"
+          new_table_width_or_height_value = gets.chomp.to_s.to_i
+          if new_table_width_or_height_value > 0
+            update_grid_value(table, width_or_height_string, new_table_width_or_height_value)
+            grid_edit_mode = false
+          end
+        end
+      end
+    end
+  end
+
 end
